78b01746d3bc8df8ed1261810edf6863eff6620a
Dimitrije <dimitrije219@gmail.com>
2025-04-07 10:18:48 +0200

    test

--- obj/Debug/net8.0/WebShop.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebShop.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebShop.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eec715ce2c1f47cf8c802feab3b410a43c39f446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6573969ead1b575aa0db770f9a2eca514b374479")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebShop.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebShop.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
